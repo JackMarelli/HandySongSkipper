cf602009a3f7d81ec9f9bb177a87a3797832a182
JackMarelli <marelligiacomo@gmail.com>
2021-08-16 16:23:38 +0200

    scambio file
    scambiato progetto pycharm rotto con progetto funzionante (con tutte le librerie incluse), aggiunto index.html vuoto per test

--- pycharm project/HandySongSkipper/main.py
+++ pycharm project/HandySongSkipper/main.py
@@ -3,11 +3,12 @@ import cv2
 import win32api
 import time
 
+PLAY_PAUSE_KEY = 0xB3
+
 capture = cv2.VideoCapture(0)
 mpDraw = mp.solutions.drawing_utils
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
-
 currentTime = 0;
 previousTime = 0;
 
@@ -18,9 +19,14 @@ while True:
     results = hands.process(rgbImg)
 
     #draw landmarks & connections
-    if results.multi_hand_landmarks :
+    if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    #call general play-pause button if something is detected and wait 2s
+    if results.multi_hand_landmarks:
+        win32api.keybd_event(PLAY_PAUSE_KEY, win32api.MapVirtualKey(PLAY_PAUSE_KEY, 0))
+        time.sleep(2)
 
     #draw fps
     currentTime = time.time()
@@ -31,3 +37,5 @@ while True:
     #show image
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
+
